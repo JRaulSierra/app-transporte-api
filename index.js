d7a3d27bdf461aa526e5e518d8007da38097986e
Raul Sierra <j.raulsierra.uni@gmail.com>
2022-07-07 19:57:13 -0600

    endpoint para registro desde mi index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hola desde nuestra app 🐱‍👤');
 })
 
+app.post('/signup', (req,res) => {
+    res.send('Endpoint para registro ✔✔ 🐱‍🚀')
+})
+
 app.listen(PORT, (req, res) => {
     console.log(`El servidor esta corriendo en el Puerto http:localhost:${PORT} 🐱‍🚀`);
 })
